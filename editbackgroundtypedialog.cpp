71cb047ed39245ec7694b082ea27ce44ba63b5d5
C. Wick <ch.wick@web.de>
2014-05-22 14:22:20 +0200

    fixed editing of mapbackground

--- editbackgroundtypedialog.cpp
+++ editbackgroundtypedialog.cpp
@@ -4,15 +4,21 @@
 EditBackgroundTypeDialog::EditBackgroundTypeDialog(const QString &initValue, QWidget *parent) :
     QDialog(parent),
     ui(new Ui::EditBackgroundTypeDialog),
-    mBackgroud(initValue)
+    mBackgroud("initValue")
 {
     ui->setupUi(this);
     ui->backgroundComboBox->addItem(tr("Forest"), QString("forest"));
     ui->backgroundComboBox->addItem(tr("Fog"), QString("fog"));
     ui->backgroundComboBox->addItem(tr("Clouds"), QString("clouds"));
 
-    ui->backgroundComboBox->setCurrentIndex(
-                ui->backgroundComboBox->findData(QVariant(mBackgroud)));
+    int index = ui->backgroundComboBox->findData(initValue);
+    if (index > 0) {
+        ui->backgroundComboBox->setCurrentIndex(index);
+    }
+    else {
+        ui->backgroundComboBox->setCurrentIndex(0);
+        mBackgroud = qvariant_cast<QString>(ui->backgroundComboBox->currentData());
+    }
 }
 
 EditBackgroundTypeDialog::~EditBackgroundTypeDialog()
